399c4edfc6455688fa2d25c0b7efcbf65e30435d
Gulbrandr <joshua.loerakker@gmail.com>
2022-03-09 14:43:11 -0500

    fixed swager stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,16 @@
+import dynamic from 'next/dynamic';
+import 'swagger-ui/dist/swagger-ui.css';
+import { useEffect } from 'react';
 import type { NextPage } from 'next';
-import { useRef } from 'react';
+import Swagger from 'features/Swagger/';
+import Layout from 'features/Layout';
 
-import { RedocWrapper } from 'features/Redoc/RedocWrapper';
-import openapi from 'openapi.yaml';
-import YAML from 'yaml';
-import ReactToPrint from 'react-to-print';
-
-const Home: NextPage = () => {
-  const componentRef = useRef(null);
+const SwaggerPage: NextPage = () => {
   return (
-    <>
-      {/* <ReactToPrint
-        trigger={() => (
-          <button className="bg-primary text-white w-auto p-4 h-auto fixed z-50  top-0 right-0 m-6 rounded">
-            Print this out!
-          </button>
-        )}
-        content={() => componentRef.current}
-      /> */}
-      <RedocWrapper
-        ref={componentRef}
-        spec={openapi}
-        options={{
-          theme: {
-            sidebar: { backgroundColor: '#041e42', textColor: '#fff' },
-            typography: { fontFamily: 'Open Sans' },
-          },
-        }}
-      />
-    </>
+    <Layout>
+      <Swagger />
+    </Layout>
   );
 };
 
-export default Home;
+export default SwaggerPage;
