dde26f1dee60379efbb41870d9567d479395ffac
Gulbrandr <joshua.loerakker@gmail.com>
2022-05-09 15:26:13 -0400

    updates

---
+++ pages/guides/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <iframe
+        className="w-full h-screen"
+        src="https://swims-club-ui.swimsmember.org/club/search"
+      />
+    </div>
+  );
+};
+
+export default Page;
